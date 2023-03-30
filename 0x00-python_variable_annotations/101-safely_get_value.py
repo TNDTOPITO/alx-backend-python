d94c5a664ea6c5193cf56e987bc529c37c44b1e3
Thabiso <940303tabiso@gmail.com>
2023-03-30 13:44:40 +0000

    Python - Variable Annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
-""" Annotates function params and return values """
+""" Given the parameters and the return values,
+    add type annotations to the function
+    Hint: look into TypeVar
+    def safely_get_value(dct, key, default = None):
+        if key in dct:
+            return dct[key]
+        else:
+            return default """
+from typing import Mapping, Any, Union, TypeVar
+T = TypeVar('T')
 
-from typing import Dict
 
-
-def safely_get_value(dct, key, default = None):
-    """ Duck-typing """
+def safely_get_value(
+        dct: Mapping,
+        key: Any,
+        default: Union[T, None] = None
+        ) -> Union[Any, T]:
+    """ More involved type annotations """
     if key in dct:
         return dct[key]
     else:
-        return default
+        return 
