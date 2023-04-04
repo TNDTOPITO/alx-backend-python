fcdb8b3a02c36a518556b4ed222433e78ab70759
Thabiso <940303tabiso@gmail.com>
2023-04-04 20:12:47 +0000

    task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from typing import Generator
+import asyncio
+import random
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """ Loops 10 times asyncronously, yields random num """
+
+    for i in range(10):
+        yield random.random()
+        await asyncio.sleep(1)
