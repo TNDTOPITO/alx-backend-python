87ca99d833ee84116c071861a108e3f3d28f605a
TNDTOPITO <105047011+TNDTOPITO@users.noreply.github.com>
2023-04-05 07:36:02 +0200

    Delete 0x00-MySQL_Advanced directory

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++
@@ -1,7 +0,0 @@
--- Creates user table on any data base
-
-CREATE TABLE IF NOT EXISTS USER (
-	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	email varchar(255) NOT NULL UNIQUE,
-	name varchar(255)
-)
